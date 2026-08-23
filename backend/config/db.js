d57775cc6375b06efa41c8181293a78caa3cb487
Abdirahman Mohamed <teyteyley3@gmail.com>
2026-08-23 18:58:43 +0300

    updated the db validations.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,47 +1,16 @@
 const sql = require('mssql');
 
-// const dbConfig = {
-//   server: process.env.DB_SERVER || 'localhost',
-
-//   // port: parseInt(process.env.DB_PORT) || 1433,
-
-//   database: process.env.DB_NAME || 'MemoryMateAI',
-
-//   options: {
-//     encrypt: process.env.DB_ENCRYPT === 'true',
-//     trustServerCertificate:
-//       process.env.DB_TRUST_SERVER_CERT !== 'false',
-
-//     trustedConnection: true,
-
-//     enableArithAbort: true,
-//   },
-
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     idleTimeoutMillis: 30000,
-//   },
-// };
-
 const dbConfig = {
-  server: process.env.DB_SERVER,
-
-  database: process.env.DB_NAME,
-
-  user: process.env.DB_USER,
-
-  password: process.env.DB_PASSWORD,
-
+  server: process.env.DB_SERVER || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 1433,
+  database: process.env.DB_NAME || 'MemoryMateAI',
   options: {
     encrypt: process.env.DB_ENCRYPT === 'true',
-
     trustServerCertificate:
       process.env.DB_TRUST_SERVER_CERT !== 'false',
-
+    trustedConnection: true,
     enableArithAbort: true,
   },
-
   pool: {
     max: 10,
     min: 0,
